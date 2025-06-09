40da595a807140bf6f44b64dbd2abdd5d1dcdede
hariOm <abhinav.rana@agamitechnologies.in>
2025-06-09 14:16:56 +0530

    issues

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -5,14 +5,14 @@ import './styles/style.css';
 
 
 const projects = [
-  { id: 1, title: "Portfolio Website", price: 0, isFree: true, zipPath: "/anon.zip", image: "/Anon-website.png", previewLink: "/portfolio-demo",
+  { id: 1, title: "Portfolio Website", price: 0, zipPath: "/anon.zip", image: "/Anon-website.png", previewLink: "/portfolio-demo",
      screenshots: ["/01.png",
       "/02.png",
       "/03.png",
       "/04.png",
       "/05.png",
     ] },
-  { id: 2, title: "E-commerce Template", price: 1, zipPath: "/anon", image: "/anon.zip", previewLink: "/ecommerce-demo", screenshots: ["/screenshots/2_1.jpg", "/screenshots/2_2.jpg"] },
+  { id: 2, title: "E-commerce Template", price: 1, isFree: true, zipPath: "/anon", image: "/anon.zip", previewLink: "/ecommerce-demo", screenshots: ["/screenshots/2_1.jpg", "/screenshots/2_2.jpg"] },
   // Add other projects similarly
 ];
 
